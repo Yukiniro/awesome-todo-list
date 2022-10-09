58b99371bc58aa08a2557c539114f26569b06d3c
Yukiniro <yukiniro@hotmail.com>
2022-10-09 23:06:07 +0800

    feat: update TodoItem

--- apps/react-todo/src/components/TodoItem.tsx
+++ apps/react-todo/src/components/TodoItem.tsx
@@ -1,15 +1,48 @@
+import { MouseEvent, useMemo } from "react";
+import classNames from "classnames";
+
 type TodoItemProps = {
   id: string;
   done: boolean;
   content: string;
+  onChange: (id: string, event: MouseEvent) => void;
 };
 
 function TodoItem(props: TodoItemProps) {
-  const { id, done, content } = props;
+  const { id, done, content, onChange } = props;
+  const handleClick = (event: MouseEvent) => {
+    onChange(id, event);
+  };
+
+  const checkClassName = useMemo(() => {
+    const baseClassName = "p-l-6 p-r-6 text-2xl";
+    if (done) {
+      return classNames(
+        baseClassName,
+        "text-green-200 i-material-symbols:check-box-outline"
+      );
+    } else {
+      return classNames(
+        baseClassName,
+        "text-gray-400 i-material-symbols-check-box-outline-blank"
+      );
+    }
+  }, [done]);
+
+  const textClassName = useMemo(() => {
+    const baseClassName = "text-2xl";
+    return done
+      ? classNames(baseClassName, "decoration-line-through text-gray-200")
+      : baseClassName;
+  }, [done]);
+
   return (
-    <div className="w-1/1 h-16 border flex items-center">
-      <input checked={done} type="checkbox" />
-      <label>{content}</label>
+    <div
+      onClick={handleClick}
+      className="w-1/1 h-16 border border-gray-300 border-l-0 border-r-0 flex items-center"
+    >
+      <div className={checkClassName} />
+      <span className={textClassName}>{content}</span>
     </div>
   );
 }
--- apps/react-todo/vite.config.ts
+++ apps/react-todo/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import Unocss from "unocss/vite";
+import { presetUno, presetIcons } from "unocss";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -16,6 +17,7 @@ export default defineConfig({
           },
         ],
       ],
+      presets: [presetUno(), presetIcons()],
     }),
   ],
 });
